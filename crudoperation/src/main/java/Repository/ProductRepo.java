fe3d011155cecc81153b3601571cb40548586a9c
jeevanatigre <jeevanatigre@gmail.com>
2024-11-30 13:14:16 +0530

    Deleted wrong folder structure

--- crudoperation/src/main/java/Repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import Entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<ProductEntity,Long> {
-}
